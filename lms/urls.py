b2b0662e5631cedb344ae688ccf35afc2cd80814
watergor <izhyvchyn@gmail.com>
2025-01-19 23:51:06 +0200

    add UserListView and template fotr this view

--- lms/urls.py
+++ lms/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from lms.views import index
+from lms.views import LessonListView, UserListView, CourseListView, index
 
-urlpatterns = [path("", index, name="index")]
+urlpatterns = [
+    path("", index, name="index"),
+    path("users/", UserListView.as_view(), name="user-list"),
+    path("lessons/", LessonListView.as_view(), name="lesson-list"),
+    path("courses/",CourseListView.as_view(), name="course-list"),
+]
 
 app_name = "lms"
--- lms/views.py
+++ lms/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
+from django.views import generic
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from lms.models import Lesson, Course, UserProfile, Enrollment
 
 
 @login_required
 def index(request):
     return render(request, "home/index.html", context={})
+
+
+class LessonListView(LoginRequiredMixin, generic.ListView):
+    model = Lesson
+    paginate_by = 5
+    context_object_name = "lesson_list"
+    template_name = "lms/lesson_list.html"
+
+
+class CourseListView(LoginRequiredMixin, generic.ListView):
+    model = Course
+    paginate_by = 5
+    context_object_name = "course_list"
+    template_name = "lms/course_list.html"
+
+
+class UserListView(generic.ListView):
+    model = UserProfile
+    paginate_by = 5
+    context_object_name = "user_list"
+    template_name = "lms/user_list.html"
